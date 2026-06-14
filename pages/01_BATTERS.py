5d7d8c18f562b6d6928883a968c58dae33bba141
Danish Sayyed <106588484+thesayyeddanishh@users.noreply.github.com>
2026-06-14 21:15:42 +0530

    Update 01_BATTERS.py

--- pages/01_BATTERS.py
+++ pages/01_BATTERS.py
@@ -1245,10 +1245,10 @@ with col_format_banner:
 
 with col_legend:
     legend_markdown = """
-    <p style='font-size: 20px; margin-top: 30px; text-align: right;'>
-        <span style='color: red; font-size: 30px;'>&#9679;</span> Wickets &nbsp;&nbsp;&nbsp;
-        <span style='color: royalblue; font-size: 30px;'>&#9679;</span> Boundaries &nbsp;&nbsp;&nbsp;
-        <span style='color: lightgrey; font-size: 30px;'>&#9679;</span> Others
+    <p style='font-size: 20px; margin-top: 40px; text-align: right;'>
+        <span style='color: red; font-size: 25px;'>&#9679;</span> Wickets &nbsp;&nbsp;&nbsp;
+        <span style='color: royalblue; font-size: 25px;'>&#9679;</span> Boundaries &nbsp;&nbsp;&nbsp;
+        <span style='color: lightgrey; font-size: 25px;'>&#9679;</span> Others
     </p>
     """
     st.markdown(legend_markdown, unsafe_allow_html=True)
